1d49dd0b3ea3ac2481b4f64e7caf16bcde5014d0
yang_elory <elory01@163.com>
2021-02-09 17:07:03 +0800

    本地文件上传

---
+++ build/meson-private/cmake_libev/CMakeMesonToolchainFile.cmake
@@ -0,0 +1,20 @@
+######################################
+###  AUTOMATICALLY GENERATED FILE  ###
+######################################
+
+# This file was generated from the configuration in the
+# relevant meson machine file. See the meson documentation
+# https://mesonbuild.com/Machine-files.html for more information
+
+if(DEFINED MESON_PRELOAD_FILE)
+    include("${MESON_PRELOAD_FILE}")
+endif()
+
+# Variables from meson
+set(CMAKE_SIZEOF_VOID_P "8")
+set(CMAKE_C_COMPILER "/usr/bin/cc")
+set(CMAKE_C_COMPILER_ID "GNU")
+set(CMAKE_C_COMPILER_VERSION "10.2.1")
+set(CMAKE_C_COMPILER_FORCED "1")
+set(CMAKE_C_COMPILER_WORKS "TRUE")
+
